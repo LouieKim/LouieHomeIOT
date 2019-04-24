d84f311a2618c5fb49f1ce0fe21c5ca5977ffccb
louie.kim <hyeok0724@naver.com>
2019-04-24 23:23:05 +0900

    190424 2323

---
+++ shm.c
@@ -0,0 +1,14 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+
+#include <string.h>
+#include <unistd.h>
+
+#define QUEUE_SIZE 10
+
+int main()
+{
+	printf("test");
+	return 0;
+}
